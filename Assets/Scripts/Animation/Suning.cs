0fb8ad116805a59742be8b00d00be9ea4adc4677
浩然 修 <1810786661@qq.com>
2024-11-09 12:43:55 +0800

    左下角树木动画

---
+++ Assets/Scripts/Animation/Suning.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suning : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator Sun;
+    //øÿ÷∆∫Ø ˝
+    public void TreeSun(bool SunUp)
+    {
+        if (SunUp)
+        {
+            Sun.SetTrigger("isSun");
+        }
+    }
+    void Start()
+    {
+        Sun = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //≤‚ ‘”√¿˝
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            bool SunUp = true;
+            TreeSun(SunUp);
+        }
+    }
+}
---
+++ Assets/Scripts/Animation/Sweating.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sweating : MonoBehaviour
+{
+    private Animator SweatDown;
+    public void TreeSweat(bool Sweat)
+    {
+        if (Sweat==true)
+        {
+            SweatDown.SetTrigger("isSweat");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SweatDown = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //测试用例
+        if (Input.GetKeyUp(KeyCode.Q)) {
+            bool Sweat = true;
+            TreeSweat(Sweat);
+        }
+        //        // 假设在其他脚本中有对敌方棋子进入核心区域的检测逻辑
+        //            if (IsEnemyInCore()) // 假设这是一个判断敌方棋子是否进入核心区域的方法
+        //            {
+        //                bool Sweat=true;
+        //                TreeSweat(Sweat);
+        //            }
+    }
+}
