2ed5696ab1bb6af10b7abf9cbd70ae8e6a7b2c77
1481065531 <1405324588@qq.com>
2024-11-16 17:45:31 +0800

    newnewnew

---
+++ Assets/Scripts/Script/x.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class x : MonoBehaviour
+{
+    public GameObject play;
+    public void X()
+    {
+        play.SetActive(false);
+    }
+}
